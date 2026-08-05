79ec64c8ad9549e0372a512d69cd7d4cee5af1b8
김기환[Gihwan.Kim 환경기술팀] <gihwan.kim@samyang.com>
2026-08-05 11:02:34 +0900

    fix: Restore simple reliable start.bat script

--- start.bat
+++ start.bat
@@ -2,11 +2,5 @@
 chcp 65001 > nul
 cd /d "%~dp0"
 
-:: 1. 개발 서버가 이미 떠있는지 또는 새로 띄울지 실행
-start /min cmd /c "npm run dev"
-
-:: 2. 서버 준비 대기 (2초)
-timeout /t 2 /nobreak > nul
-
-:: 3. 브라우저로 화면 진입
-start "" "http://localhost:5173"
+echo 분임조 평가 및 집계 시스템을 시작합니다...
+call npm run dev
